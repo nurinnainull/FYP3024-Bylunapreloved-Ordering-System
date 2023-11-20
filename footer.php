67749cc7bdc54de3d8503704c075a1f8b9833267
nurin ainul <inulrin1443@gmail.com>
2023-11-21 01:43:39 +0800

    first commit

---
+++ footer.php
@@ -0,0 +1,35 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php">home</a>
+         <a href="about.php">about</a>
+         <a href="shop.php">shop</a>
+         <a href="contact.php">feedback</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="login.php">login</a>
+         <a href="register.php">register</a>
+         <a href="cart.php">cart</a>
+         <a href="orders.php">orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +6011-21269259 </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="https://www.instagram.com/bylunapreloved/"> <i class="fab fa-instagram"></i> instagram </a>
+      </div>
+
+   </div>
+
+   <p class="credit"> &copy; copyright  @ <?php echo date('Y'); ?> by <span>Nurin Ainul</span> </p>
+
+</section>
